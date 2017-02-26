423ff2d6c6419f4f3959f2d9c70435e4257fe928
daniel <daniel@daniel.com>
2017-02-26 00:46:11 -0300

    Added views to create / edit post inside the site.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import *
+from .forms import PostFormulario
 
 
 def post_list(request):
@@ -11,6 +12,35 @@ def post_list(request):
 
     return render(request, template, context)
 
+
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostFormulario(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('blog.views.post_detail', post_id=post.pk)
+    else:
+        form = PostFormulario()
+        return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostFormulario(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('blog.views.post_detail', post_id=post.pk)
+    else:
+        form = PostFormulario(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
+
+
